475ddf6a3a4dfe8ff15a0e57e763d1c91a0b5ca8
Trezy <t@trezy.com>
2021-08-31 11:54:45 -0500

    refactor(getentities): reduce cognitive complexity

--- src/api/getEntities.js
+++ src/api/getEntities.js
@@ -7,6 +7,53 @@ import { getFile } from './getFile.js'
 
 
 /**
+ * Converts a list of IDs into a list of filenames
+ *
+ * @param {string[]} ids A list of IDs to be parsed
+ *
+ * @returns {string[]} An array of filenames
+ */
+function getFilenamesForIDs(ids) {
+	return ids.map(id => {
+		if (!id.endsWith('.json')) {
+			return `${id}.json`
+		}
+
+		return id
+	})
+}
+
+/**
+ * Returns a list of IDs
+ *
+ * @param {Object} options An object containing filtering options
+ * @param {string[]} [options.ids] Array of entity IDs to be returned
+ * @param {string} [options.patch] Maximum patch version to return data for
+ * @param {string} options.type Entity type to return
+ *
+ * @returns {Promise<String[]>} An array of entity IDs
+ */
+async function getIDs(options) {
+	const {
+		patch,
+		type,
+	} = options
+	let { ids } = options
+
+	// If no ids provided, get all entities
+	if (typeof ids === 'undefined') {
+		ids = await getDirectory(type, patch)
+	}
+
+	// If ids isn't an array of strings, throw an error
+	if (!Array.isArray(ids) || ids.some(item => typeof item !== 'string')) {
+		throw new TypeError(`ids must be an array of strings`)
+	}
+
+	return ids
+}
+
+/**
  * Returns data for entities
  *
  * @param {Object} options An object containing filtering options
@@ -25,33 +72,16 @@ export async function getEntities(options) {
 		patch,
 		type,
 	} = options
-	let { ids } = options
+	let ids = await getIDs(options)
 
 	if (typeof type !== 'string') {
 		throw new TypeError('type must be a string')
 	}
 
-	// If no ids provided, get all entities
-	if (typeof ids === 'undefined') {
-		ids = await getDirectory(type, patch)
-	}
-
-	// If ids isn't an array of strings, throw an error
-	if (!Array.isArray(ids) || ids.some(item => typeof item !== 'string')) {
-		throw new TypeError(`ids must be an array of strings`)
-	}
-
 	// Convert IDs to filenames
-	ids = ids.map(id => {
-		if (!id.endsWith('.json')) {
-			return `${id}.json`
-		}
+	const FILENAMES = getFilenamesForIDs(ids)
 
-		return id
-	})
-
-	// If ids is an array of strings, get
-	return Promise.all(ids.map(filename => {
+	return Promise.all(FILENAMES.map(filename => {
 		return getFile(`${type}/${filename}`, patch)
 	}))
 }
