3efeea5cf3bdd5511219ae2f8628b409f14d7c38
Trezy <t@trezy.com>
2021-09-13 17:30:04 -0500

    style(getskills): abstract `getSkills` to decrease cognitive complexity

--- src/getSkills.js
+++ src/getSkills.js
@@ -11,67 +11,27 @@ import {
 
 
 /**
- * Loops through an array of skills and parses enumerable properties into their
- * representative strings
+ * Gets data for a subset of skills belonging to a specific list of Pokémon
  *
- * @param {Skill[]} skills
- */
-async function parseSkillEnums(skills) {
-	const [
-		pokemonSkillSlots,
-		pokemonSkillTypes,
-	] = await Promise.all([
-		getEnums({ type: 'pokemon-skill-slot' }),
-		getEnums({ type: 'pokemon-skill-type' }),
-	])
-
-	skills.forEach(skill => {
-		skill.slot = pokemonSkillSlots[skill.slot]
-		skill.type = pokemonSkillTypes[skill.type]
-	})
-}
-
-/**
- * Returns data for held items
- *
- * @memberof module:@pokebag/data-sdk
  * @param {Object} [options={}] An object containing filtering options
  * @param {string[]} [options.ids] Array of item IDs to be returned
- * @param {boolean} [options.parseEnums=true] Whether or not parse enumerable properties
  * @param {string} [options.patch] Maximum patch version to return data for
  * @param {string[]} [options.pokemonIDs] Array of Pokémon IDs to whom the returned skills must belong
  *
  * @returns {Promise<Skill[]>} An array containing data for each item requested
  */
-export async function getSkills(options = {}) {
+async function getSkillsByPokemonIDs(options) {
 	const {
-		parseEnums = true,
+		ids,
 		patch,
 		pokemonIDs,
 	} = options
-
-	if ((typeof parseEnums !== 'undefined') && (typeof parseEnums !== 'boolean')) {
-		throw new TypeError('parseEnums must be a boolean')
-	}
-
-	if (typeof pokemonIDs === 'undefined') {
-		const SKILLS = await getEntities({
-			...(options || {}),
-			type: 'skills',
-		})
-
-		if (parseEnums) {
-			await parseSkillEnums(SKILLS)
-		}
-
-		return SKILLS
-	}
 
 	if (!Array.isArray(pokemonIDs)) {
 		throw new TypeError('pokemonIDs must be an array')
 	}
 
-	if (typeof options.ids !== 'undefined') {
+	if (typeof ids !== 'undefined') {
 		throw new TypeError('ids and pokemonIDs may not be used together; you must choose one')
 	}
 
@@ -95,9 +55,66 @@ export async function getSkills(options = {}) {
 		index += 1
 	}
 
-	if (parseEnums) {
-		await parseSkillEnums(response)
+	return response
+}
+
+/**
+ * Loops through an array of skills and parses enumerable properties into their
+ * representative strings
+ *
+ * @param {Skill[]} skills
+ */
+async function parseSkillEnums(skills) {
+	const [
+		pokemonSkillSlots,
+		pokemonSkillTypes,
+	] = await Promise.all([
+		getEnums({ type: 'pokemon-skill-slot' }),
+		getEnums({ type: 'pokemon-skill-type' }),
+	])
+
+	skills.forEach(skill => {
+		skill.slot = pokemonSkillSlots[skill.slot]
+		skill.type = pokemonSkillTypes[skill.type]
+	})
+}
+
+/**
+ * Gets data for skills
+ *
+ * @memberof module:@pokebag/data-sdk
+ * @param {Object} [options={}] An object containing filtering options
+ * @param {string[]} [options.ids] Array of skill IDs to be returned
+ * @param {boolean} [options.parseEnums=true] Whether or not parse enumerable properties
+ * @param {string} [options.patch] Maximum patch version to return data for
+ * @param {string[]} [options.pokemonIDs] Array of Pokémon IDs to whom the returned skills must belong
+ *
+ * @returns {Promise<Skill[]>} An array containing data for each skill requested
+ */
+export async function getSkills(options = {}) {
+	const {
+		parseEnums = true,
+		pokemonIDs,
+	} = options
+
+	if ((typeof parseEnums !== 'undefined') && (typeof parseEnums !== 'boolean')) {
+		throw new TypeError('parseEnums must be a boolean')
 	}
 
-	return response
+	let skills = null
+
+	if (typeof pokemonIDs !== 'undefined') {
+		skills = await getSkillsByPokemonIDs(options)
+	} else {
+		skills = await getEntities({
+			...options,
+			type: 'skills',
+		})
+	}
+
+	if (parseEnums) {
+		await parseSkillEnums(skills)
+	}
+
+	return skills
 }
